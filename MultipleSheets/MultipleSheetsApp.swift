1e105c07148a97d5767013925d2c0c88bdb5b475
Elaidzha1940 <eli080293@gmail.com>
2023-10-30 18:23:27 +0300

    MultipleSheets

---
+++ MultipleSheets/MultipleSheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleSheetsApp.swift
+//  MultipleSheets
+//
+//  Created by Elaidzha Shchukin on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MultipleSheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
